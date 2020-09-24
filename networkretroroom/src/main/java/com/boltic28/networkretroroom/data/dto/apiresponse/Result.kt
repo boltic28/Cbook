d21951943c1fa57e3b3347c6fa16128b2a942e8c
Siarhei.Baltrukevich <Siarhei.Baltrukevich@itechart-group.com>
2020-09-24 13:06:42 +0300

    v 0.6.22 room,retrofit,viewModel with RX(clear)

--- networkretroroom/src/main/java/com/boltic28/networkretroroom/data/dto/apiresponse/Result.kt
+++ networkretroroom/src/main/java/com/boltic28/networkretroroom/data/dto/apiresponse/Result.kt
@@ -9,69 +9,69 @@ data class Result(
 )
 
 
-
+//JSON - Response
 //{
-//    "results" : [
+//  "results" : [
 //    {
 //        "gender":"female",
 //        "name" : {
-    //        "title":"Miss",
-    //        "first":"Selma",
-    //        "last":"Jørgensen"
+//            "title":"Miss",
+//            "first":"Selma",
+//            "last":"Jørgensen"
 //          },
 //        "location":{
-    //        "street":{
-        //        "number":8600,
-        //        "name":"Porsevej"
-    //          },
-    //        "city":"Øster Assels",
-    //        "state":"Syddanmark",
-    //        "country":"Denmark",
-    //        "postcode":90500,
-    //        "coordinates":{
-        //        "latitude":"-34.4684",
-        //        "longitude":"99.6681"
-        //      },
-        //    "timezone":{
-            //    "offset":"+9:30",
-            //    "description":"Adelaide, Darwin"
-    //          }
-    //      },
+//            "street":{
+//                "number":8600,
+//                "name":"Porsevej"
+//              },
+//            "city":"Øster Assels",
+//            "state":"Syddanmark",
+//            "country":"Denmark",
+//            "postcode":90500,
+//            "coordinates":{
+//                "latitude":"-34.4684",
+//                "longitude":"99.6681"
+//              },
+//            "timezone":{
+//                "offset":"+9:30",
+//                "description":"Adelaide, Darwin"
+//              }
+//          },
 //        "email":"selma.jorgensen@example.com",
 //        "login":{
-    //        "uuid":"d44edaff-e645-4cda-9635-2c0c373e3e50",
-    //        "username":"greendog830",
-    //        "password":"chocha",
-    //        "salt":"ApwWYPex",
-    //        "md5":"8be2c11b67ce70720ffacfc81a5fa69b",
-    //        "sha1":"cfeaac0b2a3d4218bfa1a141633283bce3d5b50b",
-    //        "sha256":"6ff44d2af9a6ad2e3d3677fb3cd39a012d87a2a3d456baa933ee42bc37348b99"
-    //      },
+//            "uuid":"d44edaff-e645-4cda-9635-2c0c373e3e50",
+//            "username":"greendog830",
+//            "password":"chocha",
+//            "salt":"ApwWYPex",
+//            "md5":"8be2c11b67ce70720ffacfc81a5fa69b",
+//            "sha1":"cfeaac0b2a3d4218bfa1a141633283bce3d5b50b",
+//            "sha256":"6ff44d2af9a6ad2e3d3677fb3cd39a012d87a2a3d456baa933ee42bc37348b99"
+//          },
 //        "dob":{
-    //        "date":"1946-06-09T00:12:54.009Z",
-    //        "age":74
-    //      },
+//            "date":"1946-06-09T00:12:54.009Z",
+//            "age":74
+//          },
 //        "registered":{
-    //        "date":"2016-04-13T02:58:25.223Z",
-    //        "age":4
-    //      },
+//            "date":"2016-04-13T02:58:25.223Z",
+//            "age":4
+//          },
 //        "phone":"42928300",
 //        "cell":"20220657",
 //        "id":{
-    //        "name":"CPR",
-    //        "value":"090646-7237"
-    //      },
+//            "name":"CPR",
+//            "value":"090646-7237"
+//          },
 //        "picture":{
-    //        "large":"https://randomuser.me/api/portraits/women/23.jpg",
-    //        "medium":"https://randomuser.me/api/portraits/med/women/23.jpg",
-    //        "thumbnail":"https://randomuser.me/api/portraits/thumb/women/23.jpg"
-    //      },
+//            "large":"https://randomuser.me/api/portraits/women/23.jpg",
+//            "medium":"https://randomuser.me/api/portraits/med/women/23.jpg",
+//            "thumbnail":"https://randomuser.me/api/portraits/thumb/women/23.jpg"
+//          },
 //        "nat":"DK"
-//    }],
-//    "info":{
-//      "seed":"0260c38adb57f56b",
-//      "results":1,
-//      "page":1,
-//      "version":"1.3"
-//      }
+//  }],
+//  "info":{
+//    "seed":"0260c38adb57f56b",
+//    "results":1,
+//    "page":1,
+//    "version":"1.3"
+//  }
 //}
--- networkretroroom/src/main/java/com/boltic28/networkretroroom/presentation/HumanAdapter.kt
+++ networkretroroom/src/main/java/com/boltic28/networkretroroom/presentation/HumanAdapter.kt
@@ -13,7 +13,7 @@ import com.boltic28.networkretroroom.util.DiffUtilUsers
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class HumanAdapter(var items: List<Human>) : RecyclerView.Adapter<HumanAdapter.HumanHolder>() {
+class HumanAdapter(private var items: List<Human>) : RecyclerView.Adapter<HumanAdapter.HumanHolder>() {
 
     private fun setData(list: List<Human>) {
         items = list
@@ -28,15 +28,6 @@ class HumanAdapter(var items: List<Human>) : RecyclerView.Adapter<HumanAdapter.H
         result.dispatchUpdatesTo(this)
     }
 
-    fun addAll(list: List<Human>) {
-        val newData = mutableListOf<Human>()
-        newData.addAll(items)
-        newData.addAll(list)
-        items = newData
-        notifyDataSetChanged()
-    }
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         HumanHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_row, parent, false))
 
@@ -48,21 +39,21 @@ class HumanAdapter(var items: List<Human>) : RecyclerView.Adapter<HumanAdapter.H
 
     inner class HumanHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        var id: TextView = itemView.findViewById(R.id.item_id)
-        var name: TextView = itemView.findViewById(R.id.item_name)
-        var lastName: TextView = itemView.findViewById(R.id.item_last_name)
-        var email: TextView = itemView.findViewById(R.id.item_mail)
-        var age: TextView = itemView.findViewById(R.id.item_age)
-        var phone: TextView = itemView.findViewById(R.id.item_phone)
-        var gender: TextView = itemView.findViewById(R.id.item_gender)
-        var image: ImageView = itemView.findViewById(R.id.item_image)
+        private var id: TextView = itemView.findViewById(R.id.item_id)
+        private var name: TextView = itemView.findViewById(R.id.item_name)
+        private var lastName: TextView = itemView.findViewById(R.id.item_last_name)
+        private var eMail: TextView = itemView.findViewById(R.id.item_mail)
+        private var age: TextView = itemView.findViewById(R.id.item_age)
+        private var phone: TextView = itemView.findViewById(R.id.item_phone)
+        private var gender: TextView = itemView.findViewById(R.id.item_gender)
+        private var image: ImageView = itemView.findViewById(R.id.item_image)
 
 
         fun bind(item: Human) {
             id.text = item.id.toString()
             name.text = item.name
             lastName.text = item.lastName
-            email.text = item.mail
+            eMail.text = item.mail
             age.text = item.age.toString()
             phone.text = item.phone
             gender.text = item.gender.value
